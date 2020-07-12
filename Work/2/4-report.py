fd6afe8705da735e24e46d0f6b66dca043bb02f5
diegof95 <diego.botero@correounivalle.edu.co>
2020-07-11 23:36:32 -0500

    Exercise 2.4

---
+++ Work/2/4-report.py
@@ -0,0 +1,16 @@
+# report.py
+#
+# Exercise 2.4
+
+from csv import reader
+
+def read_portfolio(file_name:str) -> list:
+  """Opens a given portfolio, which is a csv file, and reads it into a list of tuples"""
+
+  portfolio = []
+  with open(file_name, 'r') as file:
+    rows = reader(file)
+    next(rows)
+    for row in rows:
+      portfolio.append((row[0], int(row[1]), float(row[2])))
+  return portfolio
